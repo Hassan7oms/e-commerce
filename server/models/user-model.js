fa63d6dbacb85b29123033d0c791c3c3cc53d3d7
Hassan7oms <93213956+Hassan7oms@users.noreply.github.com>
2025-08-15 09:45:03 +0300

    v0.2.0  userModel implementation
    usermodel is implemented
    with security hashing of passwords and comparing

---
+++ server/models/user-model.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        first: {
+            type: String,
+            required: true
+        },
+        last: {
+            type: String,
+            required: true
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['customer', 'admin'],
+        default: 'customer'
+    },
+    addresses:[{
+        label: String,
+        street: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        country: {
+            type: String,
+            required: true
+        },
+        zip: {
+            type: String,
+            required: true
+        },
+        area: {
+            type: String,
+            required: true
+        },
+        building: String,
+        apartment: String,
+        isdefault: Boolean
+
+    }]
+    
+},{timestamps:true});
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});  
+
+userSchema.methods.comparePassword = async function(inputPassword) {
+    return await bcrypt.compare(inputPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
